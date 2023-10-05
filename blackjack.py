a3d5985ad09579f31b192e6b653229c083d484ab
Tarek Chaalan <tchaalan23@icloud.com>
2023-10-05 04:52:07 -0700

    Added features to improve visibility and layout

--- blackjack.py
+++ blackjack.py
@@ -8,7 +8,13 @@
 This is a text-based blackjack game, played in the terminal.
 """
 
+from termcolor import colored
 import json
+import sys
+import time
+import termios
+import tty
+
 from game import (
     deal_initial_cards,
     play_player_turn,
@@ -17,6 +23,53 @@ from game import (
 )
 from deck import create_deck
 
+def print_slowly(text, delay=0.05):
+    """
+    Print out the text in a typewriter fashion.
+    """
+    # Disable echoing of characters
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    tty.setcbreak(sys.stdin.fileno())
+
+    try:
+        for char in text:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(delay)
+        print()
+    finally:
+        # Restore terminal settings
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+def input_slowly(prompt, delay=0.05):
+    # Disable echoing of characters
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    tty.setcbreak(sys.stdin.fileno())
+
+    try:
+        for char in prompt:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(delay)
+    finally:
+        # Restore terminal settings
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+    return input()
+
+def colorize_player_card(card):
+    """Return a colored, bold version of the card number."""
+    return colored(card, 'green', attrs=['bold'])
+
+def colorize_dealer_card(card):
+    """Return a colored, bold version of the card number."""
+    return colored(card, 'red', attrs=['bold'])
+
+def colorize_other(card):
+    """Return a colored, bold version of the card number."""
+    return colored(card, 'cyan', attrs=['bold'])
 
 def load_player_data():
     """
@@ -51,28 +104,28 @@ def display_chips(players):
     Display the current chip balances of players with chips greater than 0.
     """
     print("\n__________________________________________________\n")
-    print("\nCurrent Chip Balances:")
+    print_slowly("\nCurrent Chip Balances:")
     players_with_chips = False
     for player in players:
         if player["chips"] > 0:
-            print(player["name"] + " - Chips: " + str(player["chips"]))
+            print_slowly(player["name"] + " - Chips: " + str(player["chips"]))
             players_with_chips = True
         else:
-            topup = input(
+            topup = input_slowly(
                 player["name"]
                 + " is out of chips, an anonymous donor is offering you $10,000,"
                 + " would you like to accept? (y/n): "
             )
             while topup.lower() not in ["y", "n"]:
-                print("Invalid input. Please enter 'y' or 'n'.\n")
-                print(
+                print_slowly("Invalid input. Please enter 'y' or 'n'.\n")
+                print_slowly(
                     player["name"]
                     + " is out of chips, an anonymous donor is offering you $10,000,"
                     + " would you like to accept? (y/n): "
                 )
             if topup.lower() == "y":
                 player["chips"] = 10000
-                print(player["name"] + " - Chips: " + str(player["chips"]))
+                print_slowly(player["name"] + " - Chips: " + str(player["chips"]))
                 players_with_chips = True
     return players_with_chips
 
@@ -83,9 +136,9 @@ def get_player_names(player_count):
     """
     players = load_player_data()
     game_players = []
-    print("\n")
+    print_slowly("\n")
     for i in range(player_count):
-        player_name = input("Enter player " + str(i + 1) + "'s name: ")
+        player_name = input_slowly("Enter player " + str(i + 1) + "'s name: ")
         game_players.append(
             {
                 "name": player_name,
@@ -103,12 +156,12 @@ def get_valid_bet_amount(player):
         if player["chips"] == 0:
             return 0
         try:
-            bet = float(input(f"{player['name']}, enter your bet amount: "))
+            bet = float(input_slowly(f"{player['name']}, enter your bet amount: "))
             if 0 < bet <= player["chips"]:
                 return bet
-            print("Invalid bet amount. Please enter a valid bet.")
+            print_slowly("Invalid bet amount. Please enter a valid bet.")
         except ValueError:
-            print("Invalid input. Please enter a valid bet amount.")
+            print_slowly("Invalid input. Please enter a valid bet amount.")
 
 
 def get_player_count():
@@ -118,19 +171,19 @@ def get_player_count():
     while True:
         print("__________________________________________________\n")
         try:
-            player_count = int(input("Enter the number of players: "))
+            player_count = int(input_slowly("Enter the number of players: "))
             if player_count >= 1:
                 return player_count
-            print("\nInvalid number of players. Please try again.")
+            print_slowly("\nInvalid number of players. Please try again.")
         except ValueError:
-            print("\nInvalid input. Please enter a valid number of players.")
+            print_slowly("\nInvalid input. Please enter a valid number of players.")
 
 
 def get_bet_and_active_players(players):
     """
     Prompt the players for their bet amounts and remove players with no chips.
     """
-    print("\n")
+    print_slowly("\n")
     players_still_playing = []
     for player in players:
         bet = get_valid_bet_amount(player)
@@ -147,16 +200,16 @@ def deal_and_play_turns(players, deck, dealer_name):
     """
     Deal the initial cards and play the player and dealer turns.
     """
-    print("\n")
+    print_slowly("\n")
     for player in players:
         deal_initial_cards(player, deck)
     dealer_hand = deal_initial_cards({"name": dealer_name}, deck, True)
     # Player turns
     for player in players:
-        print("\n" + player["name"] + "'s turn (" + str(player["cardvalue"]) + ")")
+        print_slowly("\n" + player["name"] + "'s turn (" + colorize_other(str(player["cardvalue"])) + ")")
         play_player_turn(player, deck)
     # Dealer's turn
-    print("\nDealer's turn")
+    print_slowly("\nDealer's turn")
     play_dealer_turn(dealer_hand, deck)
     return dealer_hand
 
@@ -166,22 +219,22 @@ def evaluate_results_and_ask_continue(players, dealer_hand):
     Evaluate the results and ask the user if they want to continue.
     """
     print("\n__________________________________________________\n")
-    print("\nResults:\n")
-    print(
+    print_slowly("\nResults:\n")
+    print_slowly(
         dealer_hand["name"]
         + "'s Hand: "
-        + str(dealer_hand["hand"])
+        + colorize_dealer_card(str(dealer_hand["hand"]))
         + "  ("
-        + str(dealer_hand["cardvalue"])
+        + colorize_other(str(dealer_hand["cardvalue"]))
         + ")\n"
     )
     for player in players:
         evaluate_hand(player, dealer_hand)
     print("__________________________________________________")
-    play_again = input("\nDo you want to play again? (y/n): ")
+    play_again = input_slowly("\nDo you want to play again? (y/n): ")
     while play_again.lower() not in ["y", "n"]:
-        print("Invalid input. Please enter 'y' or 'n'.\n")
-        play_again = input("Do you want to play again? (y/n): ")
+        print_slowly("Invalid input. Please enter 'y' or 'n'.\n")
+        play_again = input_slowly("Do you want to play again? (y/n): ")
     return play_again.lower() == "y"
 
 
@@ -214,11 +267,11 @@ def play_game():
     save_player_data(player_data)
 
     print("\n__________________________________________________")
-    print("\nFinal Chip Balances:")
+    print_slowly("\nFinal Chip Balances:")
     for player in players:
-        print(player["name"] + " - Chips: " + str(player["chips"]))
+        print_slowly(player["name"] + " - Chips: \t" + str(player["chips"]))
     print("\n__________________________________________________")
-    print("\nThank you for playing!")
+    print_slowly("\nThank you for playing!")
     print("\n__________________________________________________")
 
 
--- game.py
+++ game.py
@@ -5,9 +5,61 @@
 """
 This file contains the functions that run the game.
 """
+from termcolor import colored
 from card import get_card_value
 from deck import draw_card
+import sys
+import time
+import termios
+import tty
 
+def print_slowly(text, delay=0.05):
+    """
+    Print out the text in a typewriter fashion.
+    """
+    # Disable echoing of characters
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    tty.setcbreak(sys.stdin.fileno())
+
+    try:
+        for char in text:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(delay)
+        print()
+    finally:
+        # Restore terminal settings
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+def input_slowly(prompt, delay=0.05):
+    # Disable echoing of characters
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    tty.setcbreak(sys.stdin.fileno())
+
+    try:
+        for char in prompt:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(delay)
+    finally:
+        # Restore terminal settings
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+    return input()
+
+def colorize_player_card(card):
+    """Return a colored, bold version of the card number."""
+    return colored(card, 'green', attrs=['bold'])
+
+def colorize_dealer_card(card):
+    """Return a colored, bold version of the card number."""
+    return colored(card, 'red', attrs=['bold'])
+
+def colorize_other(card):
+    """Return a colored, bold version of the card number."""
+    return colored(card, 'cyan', attrs=['bold'])
 
 def deal_initial_cards(player, deck, is_dealer=False):
     """
@@ -19,18 +71,18 @@ def deal_initial_cards(player, deck, is_dealer=False):
     player["cardvalue"] = get_hand_value(player["hand"])
 
     if not is_dealer:
-        print(
+        print_slowly(
             player["name"]
             + " has "
-            + card1
+            + colorize_player_card(card1)
             + " and "
-            + card2
+            + colorize_player_card(card2)
             + " ("
-            + str(player["cardvalue"])
+            + colorize_other(str(player["cardvalue"]))
             + ")"
         )
     else:
-        print("\nDealer has " + card1 + " and an unknown card")
+        print_slowly("\nDealer has " + colorize_dealer_card(card1) + (" and an") + colorize_dealer_card(" unknown card"))
 
     return player
 
@@ -65,25 +117,25 @@ def play_player_turn(player, deck):
     """
     while True:
         if player["cardvalue"] == 21:
-            print("Blackjack!")
+            print_slowly("Blackjack!")
             break
         if player["cardvalue"] > 21:
-            print("Bust!")
+            print_slowly("Bust!")
             break
 
-        action = input("Choose an action: (H)it, (S)tand, or (D)ouble: ")
+        action = input_slowly("Choose an action: (H)it, (S)tand, or (D)ouble: ")
         player["doubled"] = False
 
         if action.lower() == "h":
             card = draw_card(deck)
             player["hand"].append(card)
             player["cardvalue"] = get_hand_value(player["hand"])
-            print(
+            print_slowly(
                 player["name"]
                 + " hits and receives "
-                + card
+                + colorize_player_card(card)
                 + " ("
-                + str(player["cardvalue"])
+                + colorize_other(str(player["cardvalue"]))
                 + ")"
             )
         elif action.lower() == "d":
@@ -96,22 +148,22 @@ def play_player_turn(player, deck):
                 ]  # Subtract additional bet from player's chips
                 player["bet"] *= 2  # Double the bet
                 player["doubled"] = True
-                print(
+                print_slowly(
                     player["name"]
                     + " Doubles and receives "
-                    + card
+                    + colorize_player_card(card)
                     + ". Bet is now "
                     + str(player["bet"])
                     + " ("
-                    + str(player["cardvalue"])
+                    + colorize_other(str(player["cardvalue"]))
                     + ")"
                 )
                 break
-            print("Not enough chips to double. Please choose another action.")
+            print_slowly("Not enough chips to double. Please choose another action.")
         elif action.lower() == "s":
             break
         else:
-            print("Invalid action. Please try again.")
+            print_slowly("Invalid action. Please try again.")
 
 
 def play_dealer_turn(dealer_hand, deck):
@@ -122,30 +174,30 @@ def play_dealer_turn(dealer_hand, deck):
     dealer_card_values = [
         get_card_value(card, dealer_hand["cardvalue"]) for card in dealer_hand["hand"]
     ]
-    print(
+    print_slowly(
         "Dealer reveals his second card: "
-        + dealer_hand["hand"][1]
+        + colorize_dealer_card(dealer_hand["hand"][1])
         + " ("
-        + str(sum(dealer_card_values))
+        + colorize_other(str(sum(dealer_card_values)))
         + ")"
     )
 
     while True:
         if dealer_hand["cardvalue"] >= 17 and dealer_hand["cardvalue"] <= 21:
-            print(dealer_hand["name"] + " stands with (" + str(dealer_hand["cardvalue"]) + ")")
+            print_slowly(dealer_hand["name"] + " stands with (" + colorize_other(str(dealer_hand["cardvalue"])) + ")")
             break
         if dealer_hand["cardvalue"] > 21:
-            print(dealer_hand["name"] + " busts")
+            print_slowly(dealer_hand["name"] + " busts")
             break
         card = draw_card(deck)
         dealer_hand["hand"].append(card)
         dealer_hand["cardvalue"] = get_hand_value(dealer_hand["hand"])
-        print(
+        print_slowly(
             dealer_hand["name"]
             + " hits and receives "
-            + card
+            + colorize_dealer_card(card)
             + " ("
-            + str(dealer_hand["cardvalue"])
+            + colorize_other(str(dealer_hand["cardvalue"]))
             + ")"
         )
 
@@ -154,30 +206,30 @@ def evaluate_hand(player, dealer_hand):
     """
     This function evaluates the player and dealer's hands
     """
-    print(
+    print_slowly(
         player["name"]
         + "'s Hand: "
-        + str(player["hand"])
+        + colorize_player_card(str(player["hand"]))
         + "  ("
-        + str(player["cardvalue"])
+        + colorize_other(str(player["cardvalue"]))
         + ")"
     )
 
     if player["cardvalue"] > 21:
-        print("Bust!\n")
+        print_slowly("Bust!\n")
     elif player["cardvalue"] == dealer_hand["cardvalue"]:
-        print("Push!\n")
+        print_slowly("Push!\n")
         player["chips"] += player["bet"]
     elif player["cardvalue"] == 21 and len(player["hand"]) == 2:
-        print("Blackjack!\n")
+        print_slowly("Blackjack!\n")
         player["chips"] += player["bet"] * 2
     elif player["cardvalue"] > dealer_hand["cardvalue"] and player["doubled"]:
-        print("Won with a Double!\n")
+        print_slowly("Won with a Double!\n")
         player["chips"] += player["bet"] * 2  # Double the winnings if player doubled
     elif (
         player["cardvalue"] > dealer_hand["cardvalue"] or dealer_hand["cardvalue"] > 21
     ):
-        print("Win!\n")
+        print_slowly("Win!\n")
         player["chips"] += player["bet"] * 2
     elif player["cardvalue"] < dealer_hand["cardvalue"]:
-        print("Lose!\n")
+        print_slowly("Lose!\n")
